6393c93f32d0fd721f6ec66a2fff25998e89dccf
Murad <uddin.md.jashim@gmail.com>
2020-12-01 20:35:02 -0500

    initial commit

---
+++ src/main/java/com/example/semantic/release/semantic/SemanticApplication.java
@@ -0,0 +1,13 @@
+package com.example.semantic.release.semantic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemanticApplication.class, args);
+    }
+
+}
